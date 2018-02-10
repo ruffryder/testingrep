685ce9a29d8f362db27dc4596a861619a2666399
Christian Dimitrov <christian_dimitrov@abv.bg>
2018-02-10 21:11:16 +0200

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('/admin/users', 'AdminUsersController');
+
+Route::get('/admin', function(){
+
+    return view('admin.index');
+
+});
--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'is_active',
     ];
 
     /**
@@ -23,4 +23,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
+
+
 }
